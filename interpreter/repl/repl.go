75d36674214a880e460020aa668ffdb012e426b9
Ian Gregson <ian@gregson.me>
2020-04-22 15:30:16 +0000

    started a repl

---
+++ interpreter/repl/repl.go
@@ -0,0 +1,33 @@
+// repl/repl.go
+
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey-go/interpreter/lexer"
+	"monkey-go/interpreter/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for t := l.NextToken(); t.Type != token.EOF; t = l.NextToken() {
+			fmt.Printf("%+v\n", t)
+		}
+	}
+}
