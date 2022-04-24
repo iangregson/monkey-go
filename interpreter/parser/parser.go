b0e1b1eed5b9e3cd9d48b973cc3dc7b43f81d0fd
ian <ian@gregson.me>
2022-04-24 23:00:54 +0100

    First steps to reinstate the parser

---
+++ interpreter/parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"monkey-go/interpreter/ast"
+	"monkey-go/interpreter/lexer"
+	"monkey-go/interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens so current and peek are both set from the get go
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
