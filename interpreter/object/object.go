5616dd080ae87a2a48a43f9a39dea1c89e2da1aa
ian <ian@gregson.me>
2022-06-14 22:29:43 +0100

    add eval of let statements

--- interpreter/object/object.go
+++ interpreter/object/object.go
@@ -49,3 +49,22 @@ type Null struct{}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
+
+type Environment struct {
+	store map[string]Object
+}
+
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s}
+}
+
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	return obj, ok
+}
+
+func (e *Environment) Set(name string, obj Object) Object {
+	e.store[name] = obj
+	return obj
+}
--- interpreter/repl/repl.go
+++ interpreter/repl/repl.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"monkey-go/interpreter/evaluator"
 	"monkey-go/interpreter/lexer"
+	"monkey-go/interpreter/object"
 	"monkey-go/interpreter/parser"
 )
 
@@ -28,6 +29,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -48,7 +50,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
